9bd60cc2dc4a57612023637d843ee010bc63eb5a
“samuel” <samtorez84@gmail.com>
2024-05-28 22:42:32 -0600

    Aguegué menús y visualización de materias desde .json

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ import path from "path";
 import {fileURLToPath} from "url";
 import dotenv from "dotenv";
 import login from "./autentificacion.js"
+import data from "./data.json" assert { type: "json" };
 
 dotenv.config();
 
 const port = process.env.PORT || 8000
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
+const asesorias = data;
 
 app.use(express.json());
 
 app.use(express.static(__dirname + "/public"));
+
+//Usar EJS
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "layouts"))
 
 app.listen(port, () =>{
     console.log(`Conect http:localhost:${port}`)
@@ -30,4 +36,23 @@ app.post("/api/login", login);
 
 app.get("/registrer", (req, res) =>{
     res.sendFile(__dirname + "/layouts/iniciar_sesion.html");
+});
+
+
+//Vistas de prueba
+app.get("/testEstudiantes", (req,res) => {
+    res.render("mainEstudiantes")
+})
+
+app.get("/testAsesores", (req,res) => {
+    res.render("mainAsesores")
+})
+
+app.get("/mensajes", (req,res) => {
+    res.render("mensajes")
+})
+
+app.get("/asesorias", (req, res) => {
+    const asesorias = data.asesorias;
+    res.render("asesorias", { asesorias });
 });
